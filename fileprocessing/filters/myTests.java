8af4f4bfd858dac588d167e92c972b51519a7ed7
mohrw <mohr.wenger669@gmail.com>
2018-06-01 12:31:09 +0300

    started working on factory

--- fileprocessing/filters/myTests.java
+++ fileprocessing/filters/myTests.java
@@ -7,22 +7,32 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class myTests {
 
     public static void main (String [] args) {
         String path = "C:\\Users\\mohrw\\Desktop\\degree\\second semester\\oop\\oop-ex5\\fileprocessing\\filters\\myTester.txt";
 
-        try (FileReader file = new FileReader(path)) {
-            BufferedReader buffer = new BufferedReader(file);
-            Scanner s = new Scanner(buffer);
-            Pattern mySeq = Pattern.compile("[a-z]+#");
-            Matcher findSeq = mySeq.matcher();
-            System.out.println(s.next());
-            System.out.println(s.next().split("#"));
+//        try (FileReader file = new FileReader(path)) {
+//            BufferedReader buffer = new BufferedReader(file);
+//            Scanner s = new Scanner(buffer);
+//            String text = s.next();
+//            Pattern mySeq = Pattern.compile("([A-Z]|[a-z]+)#(\\d+)");
+//            Matcher findSeq = mySeq.matcher(text);
+//            while (findSeq.find()) {
+//                System.out.println(s.next().substring(findSeq.start(), findSeq.end()));
+//
+//            }
+//
+//        }
+//        catch (IOException n) {
+//            System.out.println("error");
+//        }
+        String test = "greater_Than#3#42";
+        Pattern mySeq = Pattern.compile("(\\w+)#(\\d+)");
+        Matcher findSeq = mySeq.matcher(test);
+        if (findSeq.find()) {
+            System.out.println(findSeq.group(1)+findSeq.group(2));
         }
-        catch (IOException n) {
-            System.out.println("error");
-        }
-
     }
 }
