3934d0ee1c708ee77849662c3996be1b835e000d
nirkrieger <31108710+nirkrieger@users.noreply.github.com>
2018-06-05 13:06:40 +0300

    Also TitleFilter

--- filesprocessing/filters/TitleFilter.java
+++ filesprocessing/filters/TitleFilter.java
@@ -4,23 +4,28 @@ import java.io.File;
 
 public class TitleFilter implements TitleFilters {
 
-    /**This is the name we want to compare to*/
-    String name;
+	/**
+	 * This is the name we want to compare to
+	 */
+	String name;
 
+	/**
+	 * This is the constructor
+	 *
+	 * @param curName the current files name
+	 */
+	TitleFilter(String curName) {
+		name = curName;
+	}
 
-    /** This is the constructor
-     * @param curName the current files name
-     */
-    TitleFilter(String curName){
-        name = curName;
-    }
-
-    /**This doeas the actual filtering
-     * @param pathname the file
-     * @return true if the file passes the filter, false otherwise
-     */
-    @Override
-    public boolean accept(File pathname) {
-        return pathname.getName().equals(name);
-    }
+	/**
+	 * This doeas the actual filtering
+	 *
+	 * @param pathname the file
+	 * @return true if the file passes the filter, false otherwise
+	 */
+	@Override
+	public boolean accept(File pathname) {
+		return pathname.getName().equals(name);
+	}
 }
